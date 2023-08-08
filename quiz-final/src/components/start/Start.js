0798f669ab25a143c8d72882efe0bf62b40f42ac
Artem <artem.uholkov@ukr.net>
2023-08-08 14:14:01 +0300

    start ready

---
+++ quiz-final/src/components/start/Start.js
@@ -0,0 +1,19 @@
+import './start.css';
+
+function Start() {
+  return (
+    <div className="main-box">
+      <p className="progression-wrapper">
+        <p className="progression" style={{ width: '50%' }}></p>
+      </p>
+      <p className="question"></p>
+      <p className="answers-list">
+        <p className="aswer"></p>
+        <p className="aswer"></p>
+        <p className="aswer"></p>
+      </p>
+    </div>
+  );
+}
+
+export default Start;
